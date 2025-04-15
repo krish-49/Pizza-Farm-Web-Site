0eb5640058298144084f5ca42322a8a3dcc7f4f2
krish-49 <krishrathod5696@gmail.com>
2025-04-15 08:42:35 +0530

    init commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your food here</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum, ipsam? Minima cumque soluta dolorum cupiditate ab, quibusdam tempore itaque eaque optio aliquid vero maxime, ipsa illo vel distinctio deleniti reprehenderit!</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
